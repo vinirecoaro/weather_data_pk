c6dce0a2ccd3532ac270b7269ceaededcb214b8d
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-18 20:19:51 -0300

    Created function to get data from API

--- lib/weather_data_pk.dart
+++ lib/weather_data_pk.dart
@@ -1,3 +1,39 @@
 library weather_data_pk;
 
-class WeatherData {}
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logger/logger.dart';
+import 'package:weather_data_pk/model/weathers.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherDataService {
+  static const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+  static String? _apiKey;
+  static final Logger logger = Logger();
+
+  static Future<void> initialize() async {
+    if (_apiKey == null) {
+      await dotenv.load(fileName: ".env");
+      _apiKey = dotenv.env['API_KEY'];
+    }
+    if (_apiKey == null) {
+      throw Exception("API_KEY not found in .env file");
+    }
+
+    _initializeService(_apiKey!);
+  }
+
+  static void _initializeService(String apiKey) {
+    logger.i('Service initialized with API key: $_apiKey');
+  }
+
+  static Future<Weathers> getWeather(double lat, double lon) async {
+    Uri url = Uri.parse("${baseUrl}lat=$lat&lon=$lon&appid=$_apiKey");
+    http.Response res = await http.get(url);
+    String resultStr = res.body;
+    Map<String, dynamic> result = jsonDecode(resultStr);
+    Weathers weather = Weathers.fromJson(result);
+    return weather;
+  }
+}
--- test/weather_data_pk_test.dart
+++ test/weather_data_pk_test.dart
@@ -4,6 +4,6 @@ import 'package:weather_data_pk/weather_data_pk.dart';
 
 void main() {
   test('adds one to input values', () {
-    final weatherData = WeatherData();
+    final weatherData = WeatherDataService();
   });
 }
