995ad324af08b547b86bd80a33380c74c6a480dd
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-19 20:03:34 -0300

    Removed api key

--- lib/weather_data_pk.dart
+++ lib/weather_data_pk.dart
@@ -3,14 +3,13 @@ library weather_data_pk;
 import 'dart:convert';
 import 'package:weather_data_pk/model/weathers.dart';
 import 'package:http/http.dart' as http;
-import 'package:weather_data_pk/util/key.dart';
 
 class WeatherDataService {
   static const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
-  static const String _apiKey = Key.API_KEY;
 
-  static Future<Weathers> getWeather(double lat, double lon) async {
-    Uri url = Uri.parse("${baseUrl}lat=$lat&lon=$lon&appid=$_apiKey");
+  static Future<Weathers> getWeather(
+      String apiKey, double lat, double lon) async {
+    Uri url = Uri.parse("${baseUrl}lat=$lat&lon=$lon&appid=$apiKey");
     http.Response res = await http.get(url);
     String resultStr = res.body;
     Map<String, dynamic> result = jsonDecode(resultStr);
