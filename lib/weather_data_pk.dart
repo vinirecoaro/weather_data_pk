a550e289b1faa7f4656bd1ed17555bcc142b2529
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-06-18 22:24:15 -0300

    Created file to save keys

--- lib/weather_data_pk.dart
+++ lib/weather_data_pk.dart
@@ -1,35 +1,15 @@
 library weather_data_pk;
 
 import 'dart:convert';
-
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:logger/logger.dart';
 import 'package:weather_data_pk/model/weathers.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather_data_pk/util/key.dart';
 
 class WeatherDataService {
   static const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
-  static String? _apiKey;
-  static final Logger logger = Logger();
-
-  static Future<void> initialize() async {
-    if (_apiKey == null) {
-      await dotenv.load(fileName: ".env");
-      _apiKey = dotenv.env['API_KEY'];
-    }
-    if (_apiKey == null) {
-      throw Exception("API_KEY not found in .env file");
-    }
-
-    _initializeService(_apiKey!);
-  }
-
-  static void _initializeService(String apiKey) {
-    logger.i('Service initialized with API key: $_apiKey');
-  }
+  static String _apiKey = Key.API_KEY;
 
   static Future<Weathers> getWeather(double lat, double lon) async {
-    await initialize();
     Uri url = Uri.parse("${baseUrl}lat=$lat&lon=$lon&appid=$_apiKey");
     http.Response res = await http.get(url);
     String resultStr = res.body;
